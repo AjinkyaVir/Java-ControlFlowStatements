982d1d114aa1907bff774960b0a725acb37bf9ff
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-06-26 14:33:28 +0530

    Ajinkya Virkud - switch case example one

---
+++ src/com/company/switchstaement/Demo.java
@@ -0,0 +1,21 @@
+package com.company.switchstaement;
+
+public class Demo {
+    public static void main(String[] args) {
+        int switchValue = 4;
+        switch (switchValue){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+                System.out.println("Value was 3");
+
+            default:
+                System.out.println("Value was not 1,2 0r 3");
+                break;
+        }
+    }
+}
